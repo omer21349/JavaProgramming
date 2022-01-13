3d0185a30accb4b9f856500b210998d45d16c319
Omer Guven <omerguvennnn@hotmail.com>
2022-01-13 11:48:21 -0500

    commit everything

---
+++ src/day38_Inheritanc/car/ParkingLot.java
@@ -0,0 +1,25 @@
+package day38_Inheritanc.car;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+
+        Toyota toyota= new Toyota("Camry",2020,20000,"Gray",2397623);
+
+        Tesla tesla= new Tesla("Model S", 2022,94990,"Red",120200);
+
+        BMW bmw=new BMW("X5", 2020, 67350, "black", 12000);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
